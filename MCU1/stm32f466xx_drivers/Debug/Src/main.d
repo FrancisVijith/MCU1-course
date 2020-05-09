ea128f10ae3aae0fed7cc9d7afad9605c0fa1007
FrancisVijith <55200827+FrancisVijith@users.noreply.github.com>
2020-05-09 12:07:44 -0400

    driver development

--- MCU1/stm32f466xx_drivers/Debug/Src/main.d
+++ MCU1/stm32f466xx_drivers/Debug/Src/main.d
@@ -1,4 +1,4 @@
 Src/main.o: ../Src/main.c \
- C:/Users/Admin/Documents/MCU1\ course/MCU1/stm32f466xx_drivers/Inc/stm32446xx.h
+ C:/Users/Admin/Documents/MCU1\ course/MCU1/stm32f466xx_drivers/drivers/inc/stm32446xx.h
 
-C:/Users/Admin/Documents/MCU1\ course/MCU1/stm32f466xx_drivers/Inc/stm32446xx.h:
+C:/Users/Admin/Documents/MCU1\ course/MCU1/stm32f466xx_drivers/drivers/inc/stm32446xx.h:
